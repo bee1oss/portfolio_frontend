beae77b781610f40f30382fe9a34a0bd124f68fa
Begenc Haciyev <begahaciyew@gmail.com>
2025-02-21 21:15:10 +0300

    Sub Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import NavbarMain from "./components/navbar/NavbarMain";
-import "./App.css"
 import HeroMain from "./components/heroSection/HeroMain";
+import SubHeroSection from "./components/subHeroSection/SubHeroSection";
 
 function App() {
   return (
     <main className="font-body">
       <NavbarMain />
       <HeroMain />
+      <SubHeroSection />
     </main>
   );
 }
--- src/components/heroSection/HeroText.jsx
+++ src/components/heroSection/HeroText.jsx
@@ -5,7 +5,7 @@ function HeroText() {
     <div className="flex flex-col gap-4 h-full justify-center md:text-left sm:text-center">
         <h2 className="lg:text-2xl sm:text-xl  uppercase text-cyan ">Back-End Web Developer</h2>
         <h1 className="md:text-[2.8rem] lg:text-6xl sm:text-4xl text-orange font-bold uppercase">Begench Hajyyev</h1>
-        <p className="sm:hidden md:block">Entry level back-end developer</p>
+        <p className="text-lg mt-4 text-white">Entry level back-end developer</p>
     </div>
   )
 }
