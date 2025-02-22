c4b0973b437feface0ac87b96c184773c0375290
Begenc Haciyev <begahaciyew@gmail.com>
2025-02-22 15:56:44 +0300

    projects section is end

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import HelperSection from "./HelperSection";
 import SkillsMain from "./components/skillsSection/SkillsMain";
 import SubSkills from "./components/skillsSection/SubSkills";
 import ExperienceMain from "./components/experienceSection/ExperienceMain";
+import ProjectsMain from "./components/projectsSections/ProjectsMain";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <SkillsMain />
       <SubSkills />
       <ExperienceMain />
+      <ProjectsMain />
       <HelperSection />
     </main>
   );
