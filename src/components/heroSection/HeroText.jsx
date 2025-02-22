69751f33e4440b8d53b26612010765d1244513b4
Begenc Haciyev <begahaciyew@gmail.com>
2025-02-22 18:39:37 +0300

    front-end is end :)

--- src/components/heroSection/HeroText.jsx
+++ src/components/heroSection/HeroText.jsx
@@ -11,7 +11,7 @@ const HeroText = () => {
         viewport={{ once: false, amount: 0 }}
         className="lg:text-2xl sm:text-xl  uppercase text-lightGrey "
       >
-        Front-End Web Developer
+        Entry level back-end developer
       </motion.h2>
       <motion.h1
         variants={fadeIn("right", 0.4)}
@@ -20,8 +20,8 @@ const HeroText = () => {
         viewport={{ once: false, amount: 0 }}
         className="md:text-[2.8rem] lg:text-6xl sm:text-4xl text-orange font-bold uppercase"
       >
-        Abdelrahman <br className="sm:hidden md:block" />
-        Qassem
+        Begench <br className="sm:hidden md:block" />
+        Hajyyev
       </motion.h1>
       <motion.p
         variants={fadeIn("up", 0.6)}
@@ -30,8 +30,6 @@ const HeroText = () => {
         viewport={{ once: false, amount: 0 }}
         className="text-lg mt-4 text-white"
       >
-        A Passionate Web Developer and Instructor <br /> with 3 years of
-        experience.
       </motion.p>
     </div>
   );
