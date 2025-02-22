626ff7e2ae44f87d869705abd38404eb91adc544
Begenc Haciyev <begahaciyew@gmail.com>
2025-02-22 18:09:24 +0300

    front-end is end :)

--- src/components/experienceSection/ExperienceMain.jsx
+++ src/components/experienceSection/ExperienceMain.jsx
@@ -1,23 +1,32 @@
-//import React from 'react'
-
 import AllExperiences from "./AllExperiences";
 import ExperienceText from "./ExperienceText";
 import ExperienceTop from "./ExperienceTop";
-
+import { motion } from "framer-motion";
+import { fadeIn } from "../../framerMotion/variants";
 
 const ExperienceMain = () => {
   return (
     <div id="experience" className="max-w-[1200px] mx-auto px-4">
-      <div>
+      <motion.div
+        variants={fadeIn("down", 0)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.7 }}
+      >
         <ExperienceText />
-      </div>
-      <div>
+      </motion.div>
+      <motion.div
+        variants={fadeIn("down", 0.5)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0 }}
+      >
         <ExperienceTop />
-      </div>
+      </motion.div>
       <div className="w-full h-1 mt-4 bg-lightBrown lg:block sm:hidden"></div>
       <AllExperiences />
     </div>
-  )
-}
+  );
+};
 
-export default ExperienceMain
+export default ExperienceMain;
--- src/components/navbar/NavbarLinks.jsx
+++ src/components/navbar/NavbarLinks.jsx
@@ -14,9 +14,16 @@ const NavbarLinks = () => {
       {links.map((link, index) => {
         return (
           <li key={index} className="group">
-            <a href="#" className="cursor-pointer text-white hover:text-cyan duration-500">
-                {link.link}
-            </a>
+            <Link
+              spy={true}
+              smooth={true}
+              duration={500}
+              offset={-130}
+              to={link.section}
+              className="cursor-pointer text-white hover:text-cyan transition-all duration-500"
+            >
+              {link.link}
+            </Link>
             <div className="mx-auto bg-cyan w-0 group-hover:w-full h-[1px] transition-all duration-500"></div>
           </li>
         );
